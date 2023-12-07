6e8cc1e6194126a6b8d876f7413f3973338f49d6
Mohammed Afsal <afsalkp.mohammed@gmail.com>
2023-12-07 23:00:05 +0400

    create the feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Afsal on 07/12/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
